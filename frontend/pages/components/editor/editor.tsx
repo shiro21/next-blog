27c03aa7f4fd380985c2d52b8ca82ca1fd7f1b22
junhyeok <junhyeok403@naver.com>
2023-03-14 21:20:28 +0900

    Fix: document Err

--- frontend/pages/components/editor/editor.tsx
+++ frontend/pages/components/editor/editor.tsx
@@ -24,6 +24,8 @@ const Editor: NextPage<IEditor> = ({ htmlStr, setHtmlStr }) => {
     // 이미지 업로드 핸들러, modules 설정보다 위에 있어야 정상 적용
     const imageHandler = () => {
         // file input 임의 생성
+        if (typeof document === undefined) return;
+
         const input = document.createElement('input');
         input.setAttribute('type', 'file');
         input.setAttribute('accept', 'image/*');
