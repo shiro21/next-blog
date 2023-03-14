de194bdec1bb9171433cb7a94e8b52bca32c8da2
junhyeok <junhyeok403@naver.com>
2023-03-14 21:30:14 +0900

    Fix: document Err TEST

--- frontend/pages/components/editor/editor.tsx
+++ frontend/pages/components/editor/editor.tsx
@@ -24,39 +24,40 @@ const Editor: NextPage<IEditor> = ({ htmlStr, setHtmlStr }) => {
     // 이미지 업로드 핸들러, modules 설정보다 위에 있어야 정상 적용
     const imageHandler = () => {
         // file input 임의 생성
-        if (typeof document === undefined) return;
-
-        const input = document.createElement('input');
-        input.setAttribute('type', 'file');
-        input.setAttribute('accept', 'image/*');
-        input.click();
-
-        input.onchange = async() => {
-            const file = input.files;
-            const formData = new FormData();
-
-            if(file) formData.append("multipartFiles", file[0]);
-
-            // file 데이터 담아서 서버에 전달하여 이미지 업로드
-            await api.post("/edit/fileAdd", formData)
-            .then(edit => {
-                if(quillRef.current) {
-                    // 현재 Editor 커서 위치에 서버로부터 전달받은 이미지 불러오는 url을 이용하여 이미지 태그 추가
-                    const index = (quillRef.current.getEditor().getSelection() as RangeStatic).index;
+        if (typeof document !== "undefined") {
+            const input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+            input.click();
     
-                    const quillEditor = quillRef.current.getEditor();
-                    quillEditor.setSelection(index, 1);
+            input.onchange = async() => {
+                const file = input.files;
+                const formData = new FormData();
     
-                    quillEditor.clipboard.dangerouslyPasteHTML(
-                        index,
-                        // `<img style="width: 150px; height: 150px;" alt="helloworld" src=${edit.data.data} />`
-                        `<img src=${edit.data.data} />`
-                    );
-                }
-            })
-            .catch(err => console.log("Edit Image Err", err));
-
+                if(file) formData.append("multipartFiles", file[0]);
+    
+                // file 데이터 담아서 서버에 전달하여 이미지 업로드
+                await api.post("/edit/fileAdd", formData)
+                .then(edit => {
+                    if(quillRef.current) {
+                        // 현재 Editor 커서 위치에 서버로부터 전달받은 이미지 불러오는 url을 이용하여 이미지 태그 추가
+                        const index = (quillRef.current.getEditor().getSelection() as RangeStatic).index;
+        
+                        const quillEditor = quillRef.current.getEditor();
+                        quillEditor.setSelection(index, 1);
+        
+                        quillEditor.clipboard.dangerouslyPasteHTML(
+                            index,
+                            // `<img style="width: 150px; height: 150px;" alt="helloworld" src=${edit.data.data} />`
+                            `<img src=${edit.data.data} />`
+                        );
+                    }
+                })
+                .catch(err => console.log("Edit Image Err", err));
+    
+            }
         }
+        
     }
 
     // useMemo를 사용하지 않고 handler를 등록할 경우 타이핑 할때마다 focus가 벗어남
